d63530e9da4218bd3248a9a004869d9ab5f28801
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2019-02-04 21:33:59 +0330

    Use multi series result

--- app/scripts/widgets/influx-box.js
+++ app/scripts/widgets/influx-box.js
@@ -5,65 +5,79 @@
 
 angular.module('ginfluxApp')//
 
-    /**
-     * @ngdoc Widget
-     * @name GInfluxBoxWidgetCtrl
-     * @description Display query in a box plot
-     * 
-     */
-    .controller('GInfluxBoxWidgetCtrl', function ($scope, $controller, $sheet) {
+/**
+ * @ngdoc Widget
+ * @name GInfluxBoxWidgetCtrl
+ * @description Display query in a box plot
+ * 
+ */
+.controller('GInfluxBoxWidgetCtrl', function ($scope, $controller, $sheet) {
 
-        // extend
-        angular.extend(this, $controller('GInfluxMultilineWidgetCtrl', {
-            $scope: $scope,
-        }));
+    // extend
+    angular.extend(this, $controller('GInfluxMultilineWidgetCtrl', {
+        $scope: $scope,
+    }));
 
-        //color palete
-        this.colorPalete = ['#E8743B', '#19A979', '#ED4A7B', '#945ECF', '#13A4B4', '#525DF4', '#BF399E', '#6C8893', '#EE6868', '#2F6497', '#dc0d0e'];
+    //color palete
+    this.colorPalete = ['#E8743B', '#19A979', '#ED4A7B', '#945ECF', '#13A4B4', '#525DF4', '#BF399E', '#6C8893', '#EE6868', '#2F6497', '#dc0d0e'];
 
-        this.getValues = function (key) {
-            var query = this.getQueryByFingerprint(key.fingerPrint);
-            var columns = this.getCacheSeriesColumns(query, key.resultIndex, key.seriesIndex);
-            var values = this.getCacheSeriesValues(query, key.resultIndex, key.seriesIndex);
-            // copy column
-            var index = columns.indexOf(key.column);
-            return $sheet.copyColumns(values, [index]);
-        };
+    this.getValues = function (key) {
+        var query = this.getQueryByFingerprint(key.fingerPrint);
+        var columns = this.getCacheSeriesColumns(query, key.resultIndex, key.seriesIndex);
+        var values = this.getCacheSeriesValues(query, key.resultIndex, key.seriesIndex);
+        // copy column
+        var index = columns.indexOf(key.column);
+        return $sheet.copyColumns(values, [index]);
+    };
 
 
-        this.loadViewData = function () {
-            this.assertTrue(this.series.length > 5, 'Just 5 series is required for box plot');
-            this.data = [];
+    this.loadViewData = function () {
+        this.assertTrue(this.series.length % 5 === 0, 'Just 5 series is required for box plot');
+        this.data = [];
+
+        for(var si = 0; si < this.series.length; si+=5){
             // TODO get series map
-            var q0 = this.getValues(this.series[0]);
-            var q1 = this.getValues(this.series[1]);
-            var q2 = this.getValues(this.series[2]);
-            var q3 = this.getValues(this.series[3]);
-            var q4 = this.getValues(this.series[4]);
+            var q0 = this.getValues(this.series[si+0]);
+            var q1 = this.getValues(this.series[si+1]);
+            var q2 = this.getValues(this.series[si+2]);
+            var q3 = this.getValues(this.series[si+3]);
+            var q4 = this.getValues(this.series[si+4]);
 
             for (var i = 0; i < q0.length; i++) {
+                // get titles of config
+                var title;
+                if(this.series.length > 5){
+                    // add series title
+                    title = this.series[si+0].title;
+                    if(q0.length > 1){
+                        title = title + '->' + i;
+                    }
+                } else {
+                    title = i;
+                }
                 var serie = {
-                    label: '' + i, // TODO: get titles of config
-                    color: this.colorPalete[i % this.colorPalete.length],
-                    values: {
-                        'whisker_low': q0[i],
-                        'Q1': q1[i],
-                        'Q2': q2[i],
-                        'Q3': q3[i],
-                        'whisker_high': q4[i]
-                    }, //
+                        label: title, 
+                        color: this.colorPalete[i % this.colorPalete.length],
+                        values: {
+                            'whisker_low': q0[i],
+                            'Q1': q1[i],
+                            'Q2': q2[i],
+                            'Q3': q3[i],
+                            'whisker_high': q4[i]
+                        }, //
                 };
                 this.data.push(serie);
             }
-        };
+        }
+    };
 
-        this.loadCahrt = function () {
-            var yformat = {
+    this.loadCahrt = function () {
+        var yformat = {
                 type: this.getModelProperty('chart.y.format.type') || 'Number',
                 specifier: this.getModelProperty('chart.y.format.specifier') || '.2s'
-            };
+        };
 
-            this.option = {
+        this.option = {
                 chart: {
                     type: 'boxPlotChart',
                     // size
@@ -91,16 +105,7 @@ angular.module('ginfluxApp')//
                     x: function (d) {
                         return d.label;
                     },
-                    //                    color: function(){
-                    //                        return 'blue';
-                    //                    },
                     maxBoxWidth: 75,
-                    //                    xAxis: {
-                    //                        showMaxMin: false,
-                    //                        tickFormat: function (data, i) {
-                    //                            return $sheet.formatData(i, {type: 'ShortString'});
-                    //                        }
-                    //                    },
                     yAxis: {
                         showMaxMin: this.getModelProperty('chart.y.showMaxMin'),
                         axisLabel: this.getModelProperty('chart.y.label') || 'No label',
@@ -109,24 +114,16 @@ angular.module('ginfluxApp')//
                         }
                     }
                 }
-            };
-
-            //Set Min and Max constraints for Y1 Axsis
-            var y1Min = this.getModelProperty('chart.y.min') || 0;
-            var y1Max = this.getModelProperty('chart.y.max') || 0;
-            if (y1Min != 0 || y1Max != 0) {
-                this.option.chart.yDomain1 = [y1Min, y1Max];
-            } else {
-                this.option.chart.yDomain1 = null;
-            }
-            //Set Min and Max constraints for Y2 Axsis
-            var y2Min = this.getModelProperty('chart.y2.min') || 0;
-            var y2Max = this.getModelProperty('chart.y2.max') || 0;
-            if (y2Min != 0 || y2Max != 0) {
-                this.option.chart.yDomain2 = [y2Min, y2Max];
-            } else {
-                this.option.chart.yDomain2 = null;
-            }
         };
 
-    });
+        //Set Min and Max constraints for Y1 Axsis
+        var y1Min = this.getModelProperty('chart.y.min') || 0;
+        var y1Max = this.getModelProperty('chart.y.max') || 0;
+        if (y1Min != 0 || y1Max != 0) {
+            this.option.chart.yDomain = [y1Min, y1Max];
+        } else {
+            this.option.chart.yDomain = null;
+        }
+    };
+
+});
